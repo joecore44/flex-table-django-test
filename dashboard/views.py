18c7c80ba04cd6c2d44d8029f96ca954fb2c6450
Joe Shepard <shepard.joseph.m@gmail.com>
2022-08-26 14:53:07 -0700

    setup flextables and some data

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,6 +7,6 @@ def dashboard_with_pivot(request):
     return render(request, 'dashboard_with_pivot.html', {})
 
 def pivot_data(request):
-    dataset = Order.objects.asll()
+    dataset = Order.objects.all()
     data = serializers.serialize('json', dataset)
     return JsonResponse(data, safe=False)
